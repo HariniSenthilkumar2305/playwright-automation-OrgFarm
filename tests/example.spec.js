31ed002780d7d55798bca8ac0a7323a88c614562
HariniSenthilkumar2305 <harinisenthilkumar2305@gmail.com>
2026-06-10 16:06:36 +0530

    code changes

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,35 @@
-// @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('test', async ({ page }) => {
+  await page.goto('https://orgfarm.store/');
+  await page.getByRole('textbox', { name: 'Enter pincode' }).click();
+  await page.getByRole('textbox', { name: 'Enter pincode' }).fill('600097');
+  await page.getByRole('button', { name: 'Check' }).click();
+  await page.getByRole('button', { name: 'Shop now!' }).click();
+  const productButtons = page.locator('[id^="quick-add-"]').getByRole('button', { name: '+ Cart' });
+  const productCount = await productButtons.count();
+  const randomIndex = Math.floor(Math.random() * productCount);
+  await productButtons.nth(randomIndex).click();
+  await page.getByRole('link', { name: 'Cart 1 item' }).click();
+  await page.getByRole('textbox', { name: 'Select delivery date' }).click();
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+const tomorrow = new 
+Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const nextDate = tomorrow.getDate().toString();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+await page.locator(`td.day:not(.old):not(.disabled):has-text("${nextDate}")`).click();
+  await page.locator('select').selectOption('07:00AM-01:00PM');
+  await page.getByRole('button', { name: 'Checkout' }).click();
+  await page.locator('#customer-email').fill('TestOrgFarm@yopmail.com');
+  await page.getByRole('textbox', { name: 'Password', exact: true }).fill('Test@123');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.getByRole('textbox', { name: 'Mobile phone number' }).click();
+  await page.getByRole('textbox', { name: 'Mobile phone number' }).fill('+91 86754 23333');
+  await page.locator('div').filter({ hasText: /^Cash on Delivery \(COD\)$/ }).nth(1).click();
+  await page.getByRole('button', { name: 'Complete order' }).click();
+  await page.getByRole('link', { name: 'View account' }).click();
+  await page.getByRole('button', { name: 'Cancel', exact: true }).click();
+  await page.getByRole('button', { name: 'Proceed' }).click();
 });
